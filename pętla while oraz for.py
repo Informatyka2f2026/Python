9be2a1853ef01e1c5b06cdd8545062cccb42f95c
n3xorek <pawelek.szablowski@gmail.com>
2026-06-11 13:30:34 +0200

    Create pętla while oraz for.py

---
+++ pętla while oraz for.py
@@ -0,0 +1,32 @@
+#petla while
+x=0
+while x<10:
+    print(x)
+    x+=1
+#instrukcja powtarzajca wywietlenie danej liczby
+
+print()
+
+#ptla for
+for i in range(10): #[0,9] [0,10)
+    print(i)
+####################################
+print() #wiersz wolny
+####################################
+for i in range(1,10): #[1,9] [1,10)
+    print(i)
+####################################
+print()
+####################################
+for i in range(1,11): #[1,10] [1,11)
+    print(i)
+####################################
+
+
+#zadanie
+#napisz program ktory obliczy sume kolejnych 5 liczb naturalnych
+suma=0
+for i in range(0,5):
+    suma+=1
+    print("suma= ",suma)
+#zadanie sposob z while
