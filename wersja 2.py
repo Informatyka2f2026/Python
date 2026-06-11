93d984575b9d7cf7322c51c7048ddc338f9ee824
n3xorek <pawelek.szablowski@gmail.com>
2026-06-11 12:52:04 +0200

    Create wersja 2.py

---
+++ wersja 2.py
@@ -0,0 +1,15 @@
+#napisz program ktory sprawdzi czy liczba dzieli sie przez 2 i czy liczba wyniku dzielnia przez 5 daje reszte 1
+print("---------------------------------")
+print("program sprawdza czy liczba x dzieli sie przez 2 oraz czy liczba wyniku z dzielnia przez 5 dzaje 1")
+print("---------------------------------")
+x=int(input("podaj liczbe"))
+if x%2==0:
+    print("---------------------------------")
+    print("tak, liczba jest podzielna przez 2")
+    print("---------------------------------")
+elif x%5==:
+    print("tak, wynik z dzielnia liczby x przez 5 daje 1")
+    print("---------------------------------")
+else:
+    print("liczba nie spenia warunkow")
+    print("---------------------------------")
