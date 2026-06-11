88e04ea596737c67911a07a9e2bbec00a48fca10
n3xorek <pawelek.szablowski@gmail.com>
2026-06-11 12:51:11 +0200

    Add multiple versions of divisibility check program

---
+++ wersja 1.py
@@ -0,0 +1,92 @@
+"""
+#napisz program ktory sprawdzi czy liczba dzieli sie przez 2 i czy liczba wyniku dzielnia przez 5 daje reszte 1
+print("---------------------------------")
+print("program sprawdza czy liczba x dzieli sie przez 2 oraz czy liczba wyniku z dzielnia przez 5 dzaje 1")
+print("---------------------------------")
+x=int(input("podaj liczbe"))
+if x%2==0:
+    print("---------------------------------")
+    print("tak, liczba jest podzielna przez 2")
+    print("---------------------------------")
+else:
+    print("---------------------------------")
+    print("nie, liczba nie jest podzielna przez 2")
+    print("---------------------------------")
+if x%5==1:
+    print("tak, wynik z dzielnia liczby x przez 5 daje 1")
+    print("---------------------------------")
+else:
+    print("nie wynik z dzielnia liczby x przez 5 nie daje 1")
+    print("---------------------------------")
+"""
+"""
+#wersja 2
+#napisz program ktory sprawdzi czy liczba dzieli sie przez 2 i czy liczba wyniku dzielnia przez 5 daje reszte 1
+print("---------------------------------")
+print("program sprawdza czy liczba x dzieli sie przez 2 oraz czy liczba wyniku z dzielnia przez 5 dzaje 1")
+print("---------------------------------")
+x=int(input("podaj liczbe"))
+if x%2==0:
+    print("---------------------------------")
+    print("tak, liczba jest podzielna przez 2")
+    print("---------------------------------")
+elif x%5==:
+    print("tak, wynik z dzielnia liczby x przez 5 daje 1")
+    print("---------------------------------")
+else:
+    print("liczba nie spenia warunkow")
+    print("---------------------------------")
+"""
+"""
+#wersja 3
+#napisz program ktory sprawdzi czy liczba dzieli sie przez 2 i czy liczba wyniku dzielnia przez 5 daje reszte 1
+print("---------------------------------")
+print("program sprawdza czy liczba x dzieli sie przez 2 oraz czy liczba wyniku z dzielnia przez 5 dzaje 1")
+print("---------------------------------")
+x=int(input("podaj liczbe"))
+if x%2==0 and x%5==1:
+    print("---------------------------------")
+    print("tak, liczba jest podzielna przez 2")
+    print("---------------------------------")
+    print("tak, wynik z dzielnia liczby x przez 5 daje 1")
+    print("---------------------------------")
+else:
+    print("liczba nie spenia obu warunkow")
+    print("---------------------------------")
+"""
+"""
+#wersja 4
+#napisz program ktory sprawdzi czy liczba dzieli sie przez 2 i czy liczba wyniku dzielnia przez 5 daje reszte 1
+print("---------------------------------")
+print("program sprawdza czy liczba x dzieli sie przez 2 oraz czy liczba wyniku z dzielnia przez 5 dzaje 1")
+print("---------------------------------")
+x=int(input("podaj liczbe"))
+if x%2==0 or x%5==1:
+    print("---------------------------------")
+    print("tak, liczba spelnia przynajmniej jeden z warunkow")
+    print("---------------------------------")
+else:
+    print("liczba nie spenia ani jednego warunkow")
+    print("---------------------------------")
+"""
+"""
+#wersja 5
+#napisz program ktory sprawdzi czy liczba dzieli sie przez 2 i czy liczba wyniku dzielnia przez 5 daje reszte 1
+print("---------------------------------")
+print("program sprawdza czy liczba x dzieli sie przez 2 oraz czy liczba wyniku z dzielnia przez 5 dzaje 1")
+print("---------------------------------")
+x=float(input("podaj liczbe"))
+if x%2==0 or x%5==1:
+    print("---------------------------------")
+    print("tak, liczba spelnia przynajmniej jeden z warunkow")
+    print("---------------------------------")
+else:
+    print("liczba nie spenia ani jednego warunkow")
+    print("---------------------------------")
+"""
+
+#modulo nie dziala przy liczbach rzeczywistch trzeba uzyc liczby naturalne
+
+"""
+
+"""
